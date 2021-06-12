deea239db71e0ddeef2fa80c816008a151ea1e5c
emilydowney <80785791+emilydowney@users.noreply.github.com>
2021-06-12 17:36:04 -0500

    Change Mongoose connect

--- index.js
+++ index.js
@@ -15,8 +15,11 @@ require('./passport');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/[movieDB]', {
-   useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect('mongodb://localhost:27017/[movieDB]', {
+   //useNewUrlParser: true, useUnifiedTopology: true });
+
+   mongoose.connect('process.env.CONNECTION_URI', {
+      useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
