8019dc1f4462ab28fc1039a5ebd7e011d5fd3ff4
emilydowney <80785791+emilydowney@users.noreply.github.com>
2021-07-11 22:28:08 -0500

    Added user profile route

--- index.js
+++ index.js
@@ -127,6 +127,18 @@ app.post('/users',
       });
   });
 
+// Get information about a user
+app.get('/users/:Username', passport.authenticate('jwt', { session: false }), (req, res) => {
+  Users.findOne({ 'User.Username': req.params.Username })
+    .then((user) => {
+      res.json(user);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('Error ' + err);
+    });
+})
+
 // Change a user's information
 app.put('/users/:Username',
   [
